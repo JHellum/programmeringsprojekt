5d6cbc24a4a6408f0eaf7fcceb0090883cc4aa55
Jacob Hellum <jacobhellum@gmail.com>
2020-06-09 11:57:00 +0200

    Exercise 4 finished

--- Exercise4/src/main.c
+++ Exercise4/src/main.c
@@ -18,12 +18,6 @@
 #include "math.h"
 
 
-//#include <Windows.h>
-
-#include <unistd.h>
-
-
-
 #define FIX14_SHIFT 14
 #define FIX14_MULT(a, b) ( (a)*(b) >> FIX14_SHIFT )
 #define FIX14_DIV(a, b) ( ((a) << FIX14_SHIFT) / b )
@@ -37,6 +31,21 @@ typedef struct {
     vector_t v;
 }ball_t;
 
+void vectorinit(vector_t *v, int32_t a){
+    v->x=calccos(a);
+    v->y=calcsin(a);
+}
+
+void ballinit(ball_t *o, vector_t v, int x, int y){
+    o->x=x;
+    o->y=y;
+
+    o->x <<= 14;
+    o->y <<= 14;
+
+    o->v=v;
+}
+
 
 void rotateVector(vector_t *vec, int32_t v){
     int32_t tempx;
@@ -45,24 +54,49 @@ void rotateVector(vector_t *vec, int32_t v){
     vec->x=tempx;
 }
 
+void ballpos(ball_t *o, int x1, int y1, int x2, int y2){
 
-void ballposupdate(ball_t *o, int a, int b, int c, int d){
-   /*gotoxy(o->x,o->y);
-   printf(" ");*/
-    //o->x++;
-   // o->y++;
-if(o->x<=a || o->x>=c){
+
+
+    x1++;
+    x1<<=14;
+    x2<<=14;
+    y1++;
+    y1<<=14;
+    y2<<=14;
+
+    gotoxy(o->x>>14,o->y>>14);
+    printf(" ");
+
+    o->x += o->v.x;
+    o->y += o->v.y;
+
+    if(o->x<=x1){
+    o->x=x1+x1-o->x;
     o->v.x*=-1;
+    }
+
+    if(o->y<=y1){
+    o->y=y1+y1-o->y;
+    o->v.y*=-1;
+    }
+
+    if(o->x>=x2){
+    o->x=x2+x2-o->x;
+    o->v.x*=-1;
+    }
+
+    if(o->y>=y2){
+    o->y=y2+y2-o->y;
+    o->v.y*=-1;
+    }
+
+
+
 }
 
-if(o->y<=b || o->y>=d){
-    o->v.y*=-1;
-}
-    o->x+=o->v.x;
-    o->y+=o->v.y;
-}
 void drawball(ball_t o){
-    gotoxy(o.x,o.y);
+    gotoxy(o.x>>14,o.y>>14);
     printf("o");
 }
 
@@ -72,17 +106,18 @@ int main(void)
     clrscr();
     box(4,2,80,30,1);
     int i=0;
+
+
     vector_t v;
-    v.x=3;
-    v.y=1;
-
     ball_t o;
-    o.x=10;
-    o.y=10;
-    o.v=v;
 
-while(i<400){
-     ballposupdate(&o, 4, 2, 80, 30);
+    vectorinit(&v,300);
+    ballinit(&o, v, 10,10);
+
+
+
+while(i<600){
+     ballpos(&o,4,2,80,30);
      drawball(o);
      i++;
 }
